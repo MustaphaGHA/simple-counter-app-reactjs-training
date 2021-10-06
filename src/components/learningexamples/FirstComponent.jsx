1865720b0339a3c2037d6a7503d51a85fcf47263
Mustapha Ghannouchi <mustapha.ghannouchi@esprit.tn>
2021-10-06 08:06:42 +0100

    #1 initial commit

---
+++ src/components/learningexamples/FirstComponent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+
+//Class Component 
+export default class FirstComponent extends Component {
+  render() {
+    return (
+      <div className="firstcomponent">
+      First Component
+      </div>
+    );
+  }
+}
